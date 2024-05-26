1252797fff7b37df75b0f434f3b054c88c979f1f
Sólyom Tibor <solyomtibi2000@gmail.com>
2024-05-26 19:59:10 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://felholab4backend.azurewebsites.net:3000/api/',
-  loginRedirectURi: 'https://felholab4backend.azurewebsites.net:3000/api/login'
+  apiUrl: 'https://felholab4backend.azurewebsites.net/api/',
+  loginRedirectURi: 'https://felholab4backend.azurewebsites.net/api/login'
 };
